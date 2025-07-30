d823745751d5861799c58d6887c0ead0b64ca392
dfcut8 <dfcut8@proton.me>
2025-07-30 10:51:04 -0400

    wip

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,32 +1,22 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+@export var speed = 400.0
 
 
 func _physics_process(_delta: float) -> void:
 	velocity = Vector2(0, 0)
+	
 	if Input.is_action_pressed("player_move_right"):
-		velocity.x = SPEED
+		velocity.x = speed
 	if Input.is_action_pressed("player_move_left"):
-		velocity.x = -SPEED
+		velocity.x = -speed
 	if Input.is_action_pressed("player_move_down"):
-		velocity.y = SPEED
+		velocity.y = speed
 	if Input.is_action_pressed("player_move_up"):
-		velocity.y = -SPEED
+		velocity.y = -speed
+	
 	move_and_slide()
 	
 	var viewport_size = get_viewport_rect().size
-	if global_position.x < 0:
-		global_position.x = 0
-	if global_position.x > viewport_size.x:
-		global_position.x = viewport_size.x
-	if global_position.y < 0:
-		global_position.y = 0
-	if global_position.y > viewport_size.y:
-		global_position.y = viewport_size.y
-		
-	
-	print(viewport_size)
-	
-	#print(global_position)
+	global_position = global_position.clamp(Vector2(0,0), viewport_size)
