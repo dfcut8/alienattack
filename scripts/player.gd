f3a3ac60ef65acb9bdc0f829276d70e9bec01a40
dfcut8 <dfcut8@proton.me>
2025-07-30 09:30:08 -0400

    proper controls

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,5 +6,13 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	velocity = Vector2(100, 0)
+	velocity = Vector2(0, 0)
+	if Input.is_action_pressed("player_move_right"):
+		velocity.x = SPEED
+	if Input.is_action_pressed("player_move_left"):
+		velocity.x = -SPEED
+	if Input.is_action_pressed("player_move_down"):
+		velocity.y = SPEED
+	if Input.is_action_pressed("player_move_up"):
+		velocity.y = -SPEED
 	move_and_slide()
