f78300e8f434cb2dfa3c49305acffad06bb72ed2
dfcut8 <dfcut8@proton.me>
2025-07-30 09:45:11 -0400

    resolve warnings

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,10 +2,9 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	velocity = Vector2(0, 0)
 	if Input.is_action_pressed("player_move_right"):
 		velocity.x = SPEED
