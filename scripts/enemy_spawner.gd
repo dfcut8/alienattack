e83a93e189d5cdec26511e143fedf236af5fba06
dfcut8 <dfcut8@proton.me>
2025-07-31 09:33:41 -0400

    control life span for rocket

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var timer = $SpawnTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- scripts/rocket.gd
+++ scripts/rocket.gd
@@ -1,12 +1,15 @@
 extends Area2D
 
 
-@export var speed = 600
-@onready var notifier = $VisibleNotifier
+@export var speed: float = 600
+@export var max_life_span: float = 0.8
+
+@onready var visible_notifier: VisibleOnScreenNotifier2D = $VisibleNotifier
 
 
 func _ready() -> void:
-	notifier.screen_exited.connect(_on_screen_exited)
+	visible_notifier.screen_exited.connect(_on_screen_exited)
+	_destroy_after_time()
 
 func _process(delta: float) -> void:
 	pass
@@ -15,4 +18,9 @@ func _physics_process(delta: float) -> void:
 	global_position.x += speed * delta
 
 func _on_screen_exited() -> void:
+	queue_free()
+
+func _destroy_after_time() -> void:
+	await get_tree().create_timer(max_life_span).timeout
+	print_debug("timer fired!")
 	queue_free()
